a121422d8a98e91ef50b30b53f553344ad5a7011
brightvarghese <77091625+brightvarghese@users.noreply.github.com>
2022-09-27 18:29:44 +0530

    Initial commit for code template

---
+++ Main.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def merge_sort(array) -> None:
+  # Write code here
+
+
+# Do not change the following code
+input_data = input()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+merge_sort(data)
+print(data)
